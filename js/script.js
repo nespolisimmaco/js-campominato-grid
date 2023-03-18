a417d77b9b0e4ebafe34c6ca416d28aca6ee3841
nespolisimmaco <snespoli@yahoo.com>
2023-03-18 16:55:05 +0100

    Add: create grid with boxes when 'play' button is clicked

--- js/script.js
+++ js/script.js
@@ -2,3 +2,28 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// L'utente clicca sul bottone per generare la griglia di gioco
+const playButton = document.querySelector(".play-button");
+console.log(playButton);
+playButton.addEventListener("click", function () {
+    // Griglia
+    const grid = document.querySelector(".grid");
+    for (let i = 1; i <= 100; i++) {
+        const box = generateGridItem(i);
+        grid.append(box);
+    }
+});
+/////////////////////
+// FUNCTIONS
+/**
+ * Description genero un grid item, ossia un box da inserire nella griglia
+ * @param {number} indexNumber
+ * @returns {element} newBox
+ */
+function generateGridItem(indexNumber) {
+    const newBox = document.createElement("div");
+    newBox.classList.add("grid-item");
+    newBox.innerHTML = indexNumber;
+    return newBox;
+}
